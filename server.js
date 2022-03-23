f72beb25849f51034b506db040114d8df127acda
raven <zlat.dominic.francisc@gmail.com>
2022-03-23 03:41:07 +0200

    port fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const path = require('path');
+const { env } = require('process');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view-engine', 'ejs');
@@ -10,5 +11,5 @@ app.get('/', (req, res) => {
     res.render('main.ejs');
 });
 
-app.listen(5600);
+app.listen(5600 || process.env.PORT);
 console.log('trenulet')
