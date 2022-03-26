cab4fdd0a48ef53f7dbc03e11b1dca8843bb5b7c
raven <zlat.dominic.francisc@gmail.com>
2022-03-26 09:17:38 +0200

    Contact info added

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const { env } = require('process');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view-engine', 'ejs');
 
+
+
+
 app.get('/', (req, res) => {
     res.render('main.ejs');
 });
+
+
+app.get('*', (req, res) => {
+    res.redirect('/');
+});
+
 
 if (process.env.NODE_ENV !== 'production') {
     server.listen(5600);
