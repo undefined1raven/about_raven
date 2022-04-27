cd3b04c6351d223568ad9356da64a0f86cf86731
raven <zlat.dominic.francisc@gmail.com>
2022-04-27 04:29:51 +0300

    Final [0]

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,7 @@ function show(e, dt) {
         document.getElementById(e).style.display = 'block';
     }
 }
-const d1_r = (nvb) => {
-    return Math.round(nvb * 100) / 100;
-}
+
 function hide(e) {
     document.getElementById(e).style.display = 'none';
 }
@@ -30,6 +28,13 @@ function hide_projects_overview(){
     show('project_details_container_acx');
 }
 
+
+document.addEventListener('keyup', (e) => {
+    if(e.key == 'Escape'){
+        project_details_back_btn_click_reflex();
+    }
+});
+
 function hide_project_details(){
     show('project_details_cover');
     show('projects_cotnainer');
@@ -38,24 +43,64 @@ function hide_project_details(){
     hide('project_details_container_acx');
 }
 
-$('#project_details_back_btn').addEventListener('click', () => {
+function project_details_back_btn_click_reflex(){
     hide_project_details();
+    hide('ring_relay_details_acx');
+    hide('ring_relay_tech_tools_list');
+    hide('project_eagle_details_acx');
+    hide('project_eagle_tech_tools_list');
+    hide('project_dolores_details_acx');
+    hide('project_dolores_tech_tools_list');
+}
+
+$('#project_details_back_btn').addEventListener('click', () => {
+    project_details_back_btn_click_reflex();
 });
+
+function enable_live_deployment_btn(){
+    $('#view_live_deployment_btn').innerText = 'View Live Deployment';
+    $('#view_live_deployment_btn').style.color = '#FFF';
+    $('#view_live_deployment_btn').style.borderColor = '#000AFF';
+    $('#view_live_deployment_btn').style.backgroundColor = '#000AFF20';
+    $('#view_live_deployment_btn').addEventListener('mouseenter', () => {
+        $('#view_live_deployment_btn').style.backgroundColor = '#000AFF40';
+    });
+    $('#view_live_deployment_btn').addEventListener('mouseleave', () => {
+        $('#view_live_deployment_btn').style.backgroundColor = '#000AFF20';
+    });
+}
 
 $('#project_dolores_container').addEventListener('click', () => {
     hide_projects_overview();
-    hide('ring_relay_details_acx');
-    hide('ring_relay_tech_tools_list');
     show('project_dolores_details_acx');
     show('project_dolores_tech_tools_list');
     $('#project_details_title_l').innerText = 'Project Dolores';
+    $('#view_live_deployment_btn').innerText = 'No Live Deployment Available';
+    $('#view_live_deployment_btn').style.color = '#333';
+    $('#view_live_deployment_btn').style.borderColor = '#333';
+    $('#view_live_deployment_btn').style.backgroundColor = '#000AFF00';
+});
+
+$('#project_eagle_container').addEventListener('click', () => {
+    hide_projects_overview();
+    show('project_eagle_tech_tools_list');
+    show('project_eagle_details_acx');
+    $('#project_details_title_l').innerText = 'Project Eagle';
+    enable_live_deployment_btn();
+    $('#view_live_deployment_btn').addEventListener('click', () => {
+        window.location = 'https://vulture-uplink.com/advanced_telemetry';
+        $('#view_live_deployment_btn').innerText = 'Redirecting...';
+    });    
 });
 
 $('#ring_relay_container').addEventListener('click', () => {
     hide_projects_overview();
-    hide('project_dolores_details_acx');
-    hide('project_dolores_tech_tools_list');
     show('ring_relay_details_acx');
     show('ring_relay_tech_tools_list');
     $('#project_details_title_l').innerText = 'Ring Relay';
+    enable_live_deployment_btn();
+    $('#view_live_deployment_btn').addEventListener('click', () => {
+        window.location = 'https://ring-relay.herokuapp.com';
+        $('#view_live_deployment_btn').innerText = 'Redirecting...';
+    });    
 });
